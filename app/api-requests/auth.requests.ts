f41710494bcec16b5c0669d3f6afc6cf6f675809
ANDEV09 <lamhoangan612@gmail.com>
2026-01-18 01:56:43 +0700

    fix: update auth response interfaces to match backend structure

--- app/api-requests/auth.requests.ts
+++ app/api-requests/auth.requests.ts
@@ -1,9 +1,10 @@
 import { privateApi, publicApi } from "~/lib/axios-instance";
 import { AUTH_ENDPOINTS } from "~/constants/api.constants";
 import CookieStorage from "~/lib/cookie-storage";
+import type { A } from "node_modules/react-router/dist/development/router-5iOvts3c.mjs";
 
 export interface LoginPayload {
-  email: string;
+  username: string;
   password: string;
 }
 
@@ -18,27 +19,42 @@ export interface RegisterResponse {
   message: string;
 }
 
+export interface UserInfoResponse {
+  id: string;
+  username: string;
+  email: string;
+  balance: number;
+}
+export interface LoginResponse {
+  message: string;
+  result: {
+    userInfo: UserInfoResponse;
+    access_token: string;
+    refresh_token: string;
+  };
+}
+
 export interface RefreshTokenResponse {
   access_token: string;
   refresh_token: string;
 }
 
 const AuthApi = {
-  // login: async (payload: LoginPayload): Promise<RegisterResponse> => {
-  //   const response = await publicApi.post<RegisterResponse>(
-  //     AUTH_ENDPOINTS.LOGIN,
-  //     payload,
-  //   );
+  login: async (payload: LoginPayload): Promise<LoginResponse> => {
+    const response = await publicApi.post<LoginResponse>(
+      AUTH_ENDPOINTS.LOGIN,
+      payload,
+    );
 
-  //   CookieStorage.setItem("access_token", response.data.access_token, {
-  //     expires: 1,
-  //   });
-  //   CookieStorage.setItem("refresh_token", response.data.refresh_token, {
-  //     expires: 7,
-  //   });
+    CookieStorage.setItem("access_token", response.data.result.access_token, {
+      expires: 1,
+    });
+    CookieStorage.setItem("refresh_token", response.data.result.refresh_token, {
+      expires: 7,
+    });
 
-  //   return response.data;
-  // },
+    return response.data;
+  },
 
   register: async (payload: RegisterPayload): Promise<RegisterResponse> => {
     const response = await publicApi.post<RegisterResponse>(
