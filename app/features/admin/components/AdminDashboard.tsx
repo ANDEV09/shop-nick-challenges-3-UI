5c667e52b33c6948dfe4e11958d26bd7cede4c54
ANDEV09 <lamhoangan612@gmail.com>
2026-01-18 23:07:55 +0700

    feat(admin): add ui admin

---
+++ app/features/admin/components/AdminDashboard.tsx
@@ -0,0 +1,26 @@
+import StatCard from "~/components/shared/StatCard";
+
+export default function AdminDashboard() {
+  return (
+    <div className="flex-1 overflow-y-auto p-8">
+      <h1 className="mb-8 text-3xl font-bold text-gray-800">
+        Thống Kê Tài Khoản
+      </h1>
+
+      <div className="mb-8 grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4">
+        <StatCard value="138000" label="Số dư hiện tại" />
+        <StatCard value="363000" label="Đã rút" />
+      </div>
+
+      <h1 className="mb-8 text-3xl font-bold text-gray-800">
+        Thống Kê Đơn Nick
+      </h1>
+      <div className="mb-8 grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4">
+        <StatCard value="10" label="Nick đang bán" />
+        <StatCard value="1" label="Nick đã bán" />
+        <StatCard value="10000" label="Tổng doanh thu" />
+        <StatCard value="1" label="Doanh thu chờ" />
+      </div>
+    </div>
+  );
+}
---
+++ app/features/admin/components/AdminHeader.tsx
@@ -0,0 +1,36 @@
+import { Search, Bell, Settings, Home, Moon, Maximize2 } from "lucide-react";
+
+export default function AdminHeader() {
+  return (
+    <div className="flex items-center justify-between border-b border-gray-200 bg-white px-8 py-4 shadow-sm">
+      <div className="flex items-center gap-3">
+        <h1 className="text-xl font-bold bg-linear-to-r from-blue-600 to-blue-800 bg-clip-text text-transparent">
+          ADMIN PANNEL
+        </h1>
+      </div>
+      <div className="flex items-center gap-6">
+        <Home
+          className="cursor-pointer text-gray-400 hover:text-gray-600"
+          size={20}
+        />
+        <Moon
+          className="cursor-pointer text-gray-400 hover:text-gray-600"
+          size={20}
+        />
+        <Maximize2
+          className="cursor-pointer text-gray-400 hover:text-gray-600"
+          size={20}
+        />
+        <Bell
+          className="cursor-pointer text-gray-400 hover:text-gray-600"
+          size={20}
+        />
+        <span className="text-xs text-gray-500">Web Developer</span>
+        <Settings
+          className="cursor-pointer text-gray-400 hover:text-gray-600"
+          size={20}
+        />
+      </div>
+    </div>
+  );
+}
